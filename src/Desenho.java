5b590be0dbc8b88d8498f6694cdfa7c847ff8888
Samuel R. dos Santos <102561330+SamCodeX01@users.noreply.github.com>
2025-03-10 21:32:09 -0300

    Update Desenho.java

--- src/Desenho.java
+++ src/Desenho.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Classe para instanciar objetos do tipo Figura de acorde coma  quantidade de parametros
  * @author Expression Samuel is undefined on line 12, column 14 in Templates/Classes/Class.java.
